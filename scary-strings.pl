48cd2e0e34e3581d5cb43f2b11092a016499d4f9
John Saigle <john.saigle@gmail.com>
2017-05-10 14:55:30 -0400

    Improve formatting and add command line parameters

--- scary-strings.pl
+++ scary-strings.pl
@@ -1,17 +1,20 @@
 # Scan a directory for php files containing potentially dangerous API calls.
 # For use when performing security audits on an application's source code.
 
-# This project is free software operating using the GNUv43
+# This project is free software operating using the 
 #!/usr/bin/perl -w
 use strict;
 use warnings;
 use File::Find ();
 use File::Basename;
+use Getopt::Long qw(GetOptions);
+Getopt::Long::Configure qw(gnu_getopt);
 use vars qw/*name *dir *prune/;
 *name   = *File::Find::name;
 *dir    = *File::Find::dir;
 *prune  = *File::Find::prune;
 
+my ($wordlist, $verbose);
 my @files_to_scan;
 my @file_access_strings = (
     'fopen',
@@ -77,10 +80,10 @@ sub begins_with
 }
 sub search_file_for_scary_strings {
     my $name = shift;
-
     my $count = 0;
     my $basename = File::Basename::basename($name);
-    print "Looking in $basename for scary strings... \n";
+    my @output_lines;
+    print "Looking in $basename for scary strings... " if $verbose;
     open my $file, "<", $name or die "$!";
     while (my $line = <$file>) {
         # trim whitespace on both sides
@@ -90,13 +93,20 @@ sub search_file_for_scary_strings {
         # iterate over array and print the line nubmer where matches are found
         for my $function_name (@file_access_strings) {
             if ($line =~ /$function_name\(.*\)/) {
-                print join("\t", $basename, $function_name, $., $line), "\n"; 
-                $count++;
+                my $output_line = join("\t", $basename, $function_name, $., $line, "\n");
+                push (@output_lines, $output_line);
             }
         }
     }
-    if ($count) {
-        print "Found $count potentially dangerous function calls.\n";
+    # Print extra verbose info
+    if (@output_lines) {
+        my $count = @output_lines;
+        print "Found $count potentially dangerous function calls.\n" if $verbose;
+        foreach my $row (@output_lines) {
+            print $row;
+        }
+    } else {
+        print "All clear.\n" if $verbose;
     }
 }
 sub wanted {
@@ -106,34 +116,51 @@ sub wanted {
 
 # ============== Begin program execution ============== #
 
-my $source = $ARGV[0];
+# Print usage if incorrect input
 my $num_args = $#ARGV + 1;
 if ($num_args < 1) {
     print $num_args;
-    print "Usage: $0 <source_directory> [wordlist]\n";
+    print "Usage: $0 <source_directory> [wordlist] [-v(erbose)]\n";
     exit;
 }
 
+# Get command line params
+GetOptions(
+    'wordlist|w=s' => \$wordlist,
+    'verbose|v' => \$verbose,
+) or die "Usage: $0 -w(ordlist) WORDLIST -v(erbose)\n";
+
+# Quit if wordlist file type is invalid
+if ($wordlist) {
+    die "Wordlist file type is invalid.\n" unless (-T $wordlist);
+} 
+
+my $source = $ARGV[0];
+# Quit if source file type is invalid
+die "$source is neither a file nor a directory.\n" unless (-T $source or -d $source);
+
+# Header
 print '===>>>>>   PHP SCARY STRINGS   <<<<<===', "\n";
 print "\t", 'Source code analysis tool, Copyright (C) 2017 by John Saigle', "\n";
 print "\t", 'Analyse PHP source code for potentially dangerous APIs, or "scary strings"!', "\n";
-print 'This is free software.', "\n";
+print "\t", 'This is free software.', "\n";
+
 
 if (-T $source) {
     search_file_for_scary_strings($source);
 }
 elsif (-d $source) {
-    print "Scanning $source for PHP files... "; 
+    print "Scanning $source for PHP files... " if $verbose;
     # Traverse desired filesystems
     File::Find::find({wanted => \&wanted}, $source );
 
     die "No php files found in $source.\n" unless @files_to_scan;
 
     my $num_files = scalar @files_to_scan;
-    print "Found: ($num_files).", "\n";
+    print "Found $num_files.", "\n" if $verbose;
     foreach (@files_to_scan) {
         search_file_for_scary_strings($_);
     }
 } else {
-    die "$source is neither a file nor a directory. Exiting.\n";
+    die 
 }
