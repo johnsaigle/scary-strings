5fae882ef1ab66e8edfcc506706a05f2b3b5e2f3
John Saigle <john.saigle@gmail.com>
2017-05-10 16:40:51 -0400

    Add support for wordlists and include examples

--- scary-strings.pl
+++ scary-strings.pl
@@ -14,75 +14,33 @@ use vars qw/*name *dir *prune/;
 *dir    = *File::Find::dir;
 *prune  = *File::Find::prune;
 
-my ($wordlist, $verbose);
+my ($wordfile, $verbose); #command line params
 my @files_to_scan;
-my @file_access_strings = (
-    'fopen',
-    'readfile',
-    'file',
-    'fpassthru',
-    'gzopen',
-    'gzfile',
-    'gzpassthru',
-    'readgzfile',
-    'copy',
-    'rename',
-    'rmdir',
-    'mkdir',
-    'unlink',
-    'file_get_contents',
-    'file_put_contents',
-    'parse_ini_file'
-);
-my @db_access_strings = (
-    'mysql_query',
-    'mssql_query',
-    'pg_query'
-);
-my @dynamic_code_execution_strings = (
-    'eval',
-    'call_user_func',
-    'call_user_func_array',
-    'call_user_method',
-    'call_user_method_array',
-    'create_function'
-);
 
-my @os_command_execution_strings = (
-    'exec',
-    'passthru',
-    'popen',
-    'proc_open',
-    'shell_exec',
-    'system'
-    #TODO: include backtick regex?
-);
-my @url_redirection_strings = (
-    'http_redirect',
-    'header',
-    'HttpMessage::setResponseCode',
-    'HttpMessage::setHeaders',
-    'setResponseCode',
-    'setHeaders'
-);
-my @socket_strings = (
-    'socket_create',
-    'socket_connect',
-    'socket_write',
-    'socket_send',
-    'socket_recv',
-    'fsockopen',
-    'pfsockopen'
-);
 sub begins_with
 {
     return substr($_[0], 0, length($_[1])) eq $_[1];
 }
+
+sub make_array_from_file
+{
+    my $file = shift;
+    my @arr;
+    open my $fh, "<", $file or die "Could not open $file";
+    while (my $line = <$fh>) {
+        chomp($line);
+        push (@arr, $line);
+    }
+    return (\@arr);
+}
+
 sub search_file_for_scary_strings {
-    my $name = shift;
-    my $count = 0;
+    my $name = shift; # name of file to analyze
+    my $function_names_ref = shift;
+    my @function_names = @$function_names_ref;
     my $basename = File::Basename::basename($name);
-    my @output_lines;
+    my @output_lines; #container to hold output before printing
+
     print "Looking in $basename for scary strings... " if $verbose;
     open my $file, "<", $name or die "$!";
     while (my $line = <$file>) {
@@ -91,10 +49,12 @@ sub search_file_for_scary_strings {
         # quick check to see if this line not worth analyzing
         next if (begins_with($line, "/*") || begins_with($line, "*") || begins_with($line, "//") || begins_with($line, "}"));
         # iterate over array and print the line nubmer where matches are found
-        for my $function_name (@file_access_strings) {
-            if ($line =~ /$function_name\(.*\)/) {
-                my $output_line = join("\t", $basename, $function_name, $., $line, "\n");
-                push (@output_lines, $output_line);
+        for my $function_name (@function_names) {
+            if ($function_name) {
+                if ($line =~ /$function_name\(.*\)/) {
+                    my $output_line = join("\t", $basename, $function_name, $., $line, "\n");
+                    push (@output_lines, $output_line);
+                }
             }
         }
     }
@@ -105,31 +65,44 @@ sub search_file_for_scary_strings {
         foreach my $row (@output_lines) {
             print $row;
         }
+        return 1;
     } else {
         print "All clear.\n" if $verbose;
+        return 0;
     }
 }
+
 sub wanted {
     /(\.php|\.class\.inc)$/ &&
     push @files_to_scan, $name;
+}
+
+sub usage {
+    die "Usage: $0 SOURCE -w(ordlist) WORD_LIST -v(erbose)\n";
 }
 
 # ============== Begin program execution ============== #
 
 # Get command line params
 GetOptions(
-    'wordlist|w=s' => \$wordlist,
+    'wordlist|w=s' => \$wordfile,
     'verbose|v' => \$verbose,
-) or die "Usage: $0 -w(ordlist) WORDLIST -v(erbose)\n";
+) or usage();
 
-# Quit if wordlist file type is invalid
-if ($wordlist) {
-    die "Wordlist file type is invalid.\n" unless (-T $wordlist);
-} 
-
+# Quit if source file not specified
 my $source = $ARGV[0];
+usage() unless $source;
+
 # Quit if source file type is invalid
 die "$source is neither a file nor a directory.\n" unless (-T $source or -d $source);
+# Quit if wordlist file type is invalid
+if ($wordfile) {
+    die "Wordlist file type is invalid.\n" unless (-T $wordfile);
+} else {
+    $wordfile = './wordlists/php/all.txt';
+    print 'Using words/php/all.txt as word list.', "\n";
+}
+
 
 # Header
 print '===>>>>>   PHP SCARY STRINGS   <<<<<===', "\n";
@@ -138,8 +111,12 @@ print "\t", 'Analyse PHP source code for potentially dangerous APIs, or "scary s
 print "\t", 'This is free software. <https://github.com/johnsaigle/scary-strings>', "\n";
 
 
+# populate array based on file contents
+my ($function_names_ref) = make_array_from_file($wordfile);
+
+my $matches_found = 0;
 if (-T $source) {
-    search_file_for_scary_strings($source);
+    search_file_for_scary_strings($source, $function_names_ref);
 }
 elsif (-d $source) {
     print "Scanning $source for PHP files... " if $verbose;
@@ -151,8 +128,10 @@ elsif (-d $source) {
     my $num_files = scalar @files_to_scan;
     print "Found $num_files.", "\n" if $verbose;
     foreach (@files_to_scan) {
-        search_file_for_scary_strings($_);
+        $matches_found = 1 if search_file_for_scary_strings($_, $function_names_ref);
     }
-} else {
-    die 
+    print "No scary strings found in the file(s)!\n" unless $matches_found;
+} 
+else {
+    usage();
 }
